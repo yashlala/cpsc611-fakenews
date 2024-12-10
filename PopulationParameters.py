a1dd1b0969e325cc0508ebcf801c0536249f25bb
Yash Lala <yashlala@gmail.com>
2024-12-09 22:41:01 -0500

    refactor: add kwargs for payoff matrix.

--- PopulationParameters.py
+++ PopulationParameters.py
@@ -5,7 +5,7 @@
 
 
 class PopulationParameters:
-    def __init__(self):
+    def __init__(self, **kwargs):
         # The RGB for the colors used to visualize the population and strategies
         self.REAL_COLOR = [0.0, 0.0, 0.8]
         self.FAKE_COLOR = [0.8, 0.0, 0.0]
@@ -15,16 +15,16 @@ class PopulationParameters:
         self.NEW_FAKE_COLOR = [1.0, 0.5, 0.0]
         # The payoff matrix for the game, vectorized by rows
         self.payoff = [
-                1.0,   # 0: Real news -> real news interaction
-                0.0,   # 1: Real news -> fake news interaction
-                1.0,   # 2: Real news -> fact-checker interaction
-                -3.0,  # 3: Real news -> misinformation interaction
-                0.0,   # 4: Fake news -> real news interaction
-                1.0,   # 5: Fake news -> fake news interaction
-                -2.0,  # 6: Fake news -> fact-checker interaction
-                1.0,   # 7: Fake news -> misinformation interaction
-                0.5,   # 8: Fact-checker boost to real news nodes
-                0.5,   # 9: Misinformation boost to fake news nodes
+                kwargs.get('real_from_real', 1.0),       # 0: Real news <- real news interaction
+                kwargs.get('real_from_fake', 0.0),       # 1: Real news <- fake news interaction
+                kwargs.get('real_from_factcheck', 1.0),  # 2: Real news <- fact-checker interaction
+                kwargs.get('real_from_misinfor', -3.0),  # 3: Real news <- misinformation interaction
+                kwargs.get('fake_from_real', 0.0),       # 4: Fake news <- real news interaction
+                kwargs.get('fake_from_fake', 1.0),       # 5: Fake news <- fake news interaction
+                kwargs.get('fake_from_factcheck', -2.0), # 6: Fake news <- fact-checker interaction
+                kwargs.get('fake_from_misinfor', 1.0),   # 7: Fake news <- misinformation interaction
+                kwargs.get('factcheck_boost', 0.5),      # 8: Fact-checker boost to real news nodes
+                kwargs.get('misinfor_boost', 0.5),       # 9: Misinformation boost to fake news nodes
                 0.0,
                 0.0,
                 0.0]
